7fa4038294bdaea537ffc189f41347a03876ff6f
Yeji Ha <yeji206000@gmail.com>
2024-03-04 02:15:36 -0800

    update functions with SQL queries

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -3,29 +3,83 @@ const users = require("./json/users.json");
 
 /// Users
 
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+
+
 /**
  * Get a single user from the database given their email.
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user && user.email.toLowerCase() === email.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+// const getUserWithEmail = function (email) {
+//   let resolvedUser = null;
+//   for (const userId in users) {
+//     const user = users[userId];
+//     if (user && user.email.toLowerCase() === email.toLowerCase()) {
+//       resolvedUser = user;
+//     }
+//   }
+//   return Promise.resolve(resolvedUser);
+// }; // I left this on my own purpose! 
+
+const getUserWithEmail = function(email) {
+  const queryString = `
+    SELECT * 
+    FROM users 
+    WHERE email = $1;
+  `;
+
+  return pool.query(queryString, [email.toLowerCase()])
+    .then(res => {
+      if (res.rows.length > 0) {
+        return res.rows[0]; // User found, return the user object
+      } else {
+        return null; // No user found, return null
+      }
+    })
+    .catch(err => {
+      console.error('query error', err.stack);
+      return null; // In case of error, return null
+    });
 };
+
 
 /**
  * Get a single user from the database given their id.
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
+
+// const getUserWithId = function (id) {
+//   return Promise.resolve(users[id]);
+// }; // I left this on my own purpose! 
+
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  const queryString = `
+    SELECT * 
+    FROM users 
+    WHERE id = $1;
+  `;
+
+  return pool.query(queryString, [id])
+    .then(res => {
+      if (res.rows.length > 0) {
+        return res.rows[0]; // User found, return the user object
+      } else {
+        return null; // No user found, return null
+      }
+    })
+    .catch(err => {
+      console.error('query error', err.stack);
+      return null; // In case of error, return null
+    });
 };
 
 /**
@@ -33,11 +87,28 @@ const getUserWithId = function (id) {
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+// const addUser = function (user) {
+//   const userId = Object.keys(users).length + 1;
+//   user.id = userId;
+//   users[userId] = user;
+//   return Promise.resolve(user);
+// }; // I left this on my own purpose! 
+
+const addUser = function(user) {
+  const queryString = `
+    INSERT INTO users (name, email, password)
+    VALUES ($1, $2, $3)
+    RETURNING *;
+  `;
+
+  return pool.query(queryString, [user.name, user.email, user.password])
+    .then(res => {
+      return res.rows[0]; // Return the newly added user object
+    })
+    .catch(err => {
+      console.error('query error', err.stack);
+      return null; // In case of error, return null
+    });
 };
 
 /// Reservations
@@ -59,12 +130,16 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function (options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+const getAllProperties = (options, limit = 10) => {
+  return pool
+    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .then((result) => {
+
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 /**
